49937952fa2f63cc9522116d79fb47489ec8fb56
Ravid Pony <142820634+RavidPony@users.noreply.github.com>
2024-07-28 20:08:53 +0300

    Update and rename simple_join_solution.sql to joins_solutions.sql

--- Trips and Users/joins_solutions.sql
+++ Trips and Users/joins_solutions.sql
@@ -25,3 +25,13 @@ JOIN Users c
   ON t.Client_Id = c.Users_Id 
   And c.Banned = 'No'
 GROUP BY t.Request_at;
+
+--option 3 
+SELECT
+  Request_at AS Day
+  ,ROUND(SUM(Status != "completed") / COUNT(*), 2) AS 'Cancellation Rate'
+FROM Trips
+WHERE Request_at BETWEEN "2013-10-01" AND "2013-10-03"
+  AND Driver_Id IN (SELECT Users_Id FROM Users WHERE Banned = 'No')
+  AND Client_Id IN (SELECT Users_Id FROM Users WHERE Banned = 'No')
+GROUP BY Request_at;
