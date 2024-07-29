66c87e94a174880364a1f3f95cbcc085f7b68d2f
Ravid Pony <142820634+RavidPony@users.noreply.github.com>
2024-07-29 17:14:17 +0300

    Update window_function_solution.sql

--- The Most Frequently Ordered Products for Each Customer/window_function_solution.sql
+++ The Most Frequently Ordered Products for Each Customer/window_function_solution.sql
@@ -8,9 +8,5 @@ FROM (
 ) AS ranked_products
 WHERE rank_count = 1;
 
--- First, the GROUP BY operation is performed on customer_id, product_id, product_name, and then the RANK() operation is executed.
--- JOIN: Joining the Orders and Products tables based on the product identifier (product_id). This allows combining data about the products with the orders they are included in.
--- GROUP BY customer_id, product_id, product_name: Groups the records by three attributes - customer identity, product identifier, and product name. This allows calculating the order count for each combination of customer and product.
--- COUNT(*): At this stage, for each group created after the GROUP BY, the count of records is calculated. This gives the number of orders each customer has made for each product.
--- RANK() OVER (PARTITION BY customer_id ORDER BY COUNT(*) DESC): The RANK() function is applied after obtaining the total number of orders for each combination of customer and product. It is applied to each group (partition) of customer (customer_id), where the records are sorted by the number of orders in descending order. This returns a ranking of the products for each customer, where the product with the highest number of orders gets the highest rank (1).
--- Therefore, the RANK() function is always executed after the different groups have been created and the counts have been completed using the GROUP BY.
+-- Window functions are written in the SELECT clause but are executed after the groups are created in the GROUP BY and HAVING stages. 
+-- They are written as part of the SELECT statement, but their execution takes place after the HAVING stage and before the ORDER BY stage.
