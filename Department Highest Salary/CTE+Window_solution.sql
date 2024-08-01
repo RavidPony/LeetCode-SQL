e2efee50a6395dddfab9865b2af4904e8aa046fe
Ravid Pony <142820634+RavidPony@users.noreply.github.com>
2024-08-01 18:13:23 +0300

    Create CTE+Window_solution.sql

---
+++ Department Highest Salary/CTE+Window_solution.sql
@@ -0,0 +1,16 @@
+WITH T AS (
+        SELECT
+            d.name AS department,
+            e.name AS employee,
+            salary,
+            RANK() OVER (
+                PARTITION BY d.name
+                ORDER BY salary DESC
+            ) AS rk
+        FROM
+            Employee AS e
+            JOIN Department AS d ON e.departmentId = d.id
+    )
+SELECT department, employee, salary
+FROM T
+WHERE rk = 1;
